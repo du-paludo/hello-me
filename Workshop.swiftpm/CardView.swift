dd9c95e2308e154585782e23f142e1de1fb3307e
Eduardo Paludo <70333794+du-paludo@users.noreply.github.com>
2025-02-24 09:10:45 -0300

    workshop comentado

--- Workshop.swiftpm/CardView.swift
+++ Workshop.swiftpm/CardView.swift
@@ -5,14 +5,17 @@ struct CardView: View {
     
     var body: some View {
         ZStack {
+            // Imagem da fruta
             Image(fruit.image)
                 .resizable()
                 .scaledToFill()
                 .frame(width: 400, height: 200)
                 .clipped()
             
+            // Gradiente vertical que vai do preto ao transparente
             LinearGradient(colors: [Color.black, Color.clear], startPoint: .bottom, endPoint: .top)
             
+            // Um texto com o nome da fruta
             Text(fruit.title)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
                 .foregroundStyle(.white)
--- Workshop.swiftpm/ContentView.swift
+++ Workshop.swiftpm/ContentView.swift
@@ -1,15 +1,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Um array (vetor) de frutas
     let fruits: [FruitModel] = [
-        FruitModel(title: "Banana", image: "banana", description: "É uma banana muito amarela"),
+        FruitModel(title: "Banana", image: "banana", description: "Ba-ba-ba-ba-ba-na-na"),
         FruitModel(title: "Melancia", image: "melancia", description: "90% água"),
         FruitModel(title: "Morango", image: "morango", description: "Yummyyy"),
     ]
     
     var body: some View {
+        // Inicia uma pilha de navegação no app (deve ser instanciada apenas uma vez no topo da hierarquia)
         NavigationStack {
             VStack {
+                // Cria botões que levam às Views com detalhes de cada fruta
                 ForEach(fruits, id: \.title) { fruit in
                     NavigationLink(destination: CardDetails(fruit: fruit)) {
                         CardView(fruit: fruit)
